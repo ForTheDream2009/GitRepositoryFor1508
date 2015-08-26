cea73725fe33d033a20b9da68460448bb05a1af0
poppy <poppy@gmail.com>
2015-08-26 08:40:26 +0800

    111

---
+++ Demo2_8.26/Demo2_8.26/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Demo2_8.26
+//
+//  Created by tarena on 15/8/26.
+//  Copyright (c) 2015年 tarena. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Demo2_8.26/Demo2_8.26/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Demo2_8.26
+//
+//  Created by tarena on 15/8/26.
+//  Copyright (c) 2015年 tarena. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    NSLog(@"This is the first log");
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
